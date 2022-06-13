83571e2fdbd629b90537e4079ae6e27f97f6eeac
giuliocapo <caporrogiulio@gmail.com>
2022-06-13 18:55:38 +0200

    son

--- src/test/java/testm/TestModularArithmetic.java
+++ src/test/java/testm/TestModularArithmetic.java
@@ -13,6 +13,7 @@ public class TestModularArithmetic {
         assertEquals(true, output);
     }
 
+    //the system got a logic bug cause this test should give you true as answer
     @Test
     public void testCheckCongruenceNegative(){
         boolean output = ModularArithmetic.checkCongruence(50,5,-5);
